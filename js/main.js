3ebec1b4a2e98b2891efa53471c83f80337a687c
letsbe1 <aa1245541@gmail.com>
2023-05-15 22:41:07 +0300

    Added a bit of animations. Finished langing page

--- js/main.js
+++ js/main.js
@@ -6,6 +6,73 @@ let smoother = ScrollSmoother.create({
     content: "#content",
     smooth: 2,
     effects: true,
-    
 });
+
+function animateFrom(elem, direction) {
+    direction = direction || 1;
+    var x = 0,
+        y = direction * 100;
+    if(elem.classList.contains("gs_reveal_fromLeft")) {
+      x = -100;
+      y = 0;
+    } else if (elem.classList.contains("gs_reveal_fromRight")) {
+      x = 100;
+      y = 0;
+    }
+    elem.style.transform = "translate(" + x + "px, " + y + "px)";
+    elem.style.opacity = "0";
+    gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+      duration: 1.25, 
+      x: 0,
+      y: 0, 
+      autoAlpha: 1, 
+      ease: "expo", 
+      overwrite: "auto"
+    });
+  }
+  
+  function hide(elem) {
+    gsap.set(elem, {autoAlpha: 0});
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    gsap.registerPlugin(ScrollTrigger);
+    
+    gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
+      hide(elem);
+      
+      ScrollTrigger.create({
+        trigger: elem,
+        onEnter: function() { animateFrom(elem) }, 
+        onEnterBack: function() { animateFrom(elem, -1) },
+        onLeave: function() { hide(elem) } 
+      });
+    });
+  });
+  
+  let  = gsap.timeline({
+    // yes, we can add it to an entire timeline!
+    scrollTrigger: {
+      trigger: ".reputationSection",
+      pin: true,   // pin the trigger element while active
+      start: "top top", // when the top of the trigger hits the top of the viewport
+      end: "+=500", // end after scrolling 500px beyond the start
+      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      snap: {
+        snapTo: "labels", // snap to the closest label in the timeline
+        duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+        delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+        ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
+      }
+    }
+  });
+
+// add animations and labels to the timeline
+tl.addLabel("start")
+  .from(".box p", {scale: 0.3, rotation:45, autoAlpha: 0})
+  .addLabel("color")
+  .from(".box", {backgroundColor: "#28a92b"})
+  .addLabel("spin")
+  .to(".box", {rotation: 360})
+  .addLabel("end");
 
